562770b3b3f122df467b90e367f685ac2becd7c8
Nastss6tx5 <Nastasiia227@gmail.com>
2025-08-09 17:17:05 +0300

    feat(STOCK-109) fixer openAPI title, typePort

--- src/main/java/asset/spy/statistics/service/AssetSpyStatisticsServiceApplication.java
+++ src/main/java/asset/spy/statistics/service/AssetSpyStatisticsServiceApplication.java
@@ -1,12 +1,16 @@
 package asset.spy.statistics.service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Asset spy statistics service API",
+        description = "Rest API statistics service", version = "1.0.0"))
 public class AssetSpyStatisticsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AssetSpyStatisticsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AssetSpyStatisticsServiceApplication.class, args);
+    }
 }
